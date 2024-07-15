475f2eaa75eaf698f497e67e7899dc1902728dbd
vinoth <a.vinothk@outlook.com>
2024-07-15 10:49:33 +0530

    Fetch extra info

--- lib/omniauth/strategies/wix.rb
+++ lib/omniauth/strategies/wix.rb
@@ -3,6 +3,7 @@ require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
     class Wix < OmniAuth::Strategies::OAuth2
+      SITE_INFO_URL = 'https://www.wixapis.com/apps/v1/instance'
       option :name, 'wix'
 
       option :client_options, {
@@ -12,7 +13,7 @@ module OmniAuth
 
       option :provider_ignores_state, true
 
-      uid { request.params["instanceId"] }
+      uid { raw_info['site']['site_id'] }
 
       credentials do
         hash = {"token" => access_token.token}
@@ -21,6 +22,22 @@ module OmniAuth
         hash["expires"] = access_token.expires?
         hash
       end
+
+      info do
+        prune!({
+          'id' => raw_info['site']['site_id'],
+          'name' => raw_info['site']['site_display_name'],
+          'url' => raw_info['site']['url'],
+          'email'=> raw_info['site']['owner_email']
+        })
+      end
+
+      extra do
+        hash = {}
+        hash['raw_info'] = raw_info unless skip_info?
+        prune! hash
+      end
+
 
       def client
         ::OAuth2::Client.new(options.client_id, options.client_secret, deep_symbolize(options.client_options)) do |b|
@@ -41,6 +58,10 @@ module OmniAuth
         full_host + script_name + callback_path
       end
 
+      def raw_info
+        @raw_info ||= access_token.get(SITE_INFO_URL, headers: {"Authorization" => access_token.token}).parsed
+      end
+
       def build_access_token
         verifier = request.params["code"]
         params = {
@@ -50,6 +71,15 @@ module OmniAuth
           client_secret: options.client_secret,
         }.merge(token_params.to_hash(:symbolize_keys => true).merge({ headers: { 'Content-Type' => 'application/json' } }))
         client.auth_code.get_token(verifier, params, deep_symbolize(options.auth_token_params))
+      end
+
+      private
+
+      def prune!(hash)
+        hash.delete_if do |_, value|
+          prune!(value) if value.is_a?(Hash)
+          value.nil? || (value.respond_to?(:empty?) && value.empty?)
+        end
       end
     end
   end
